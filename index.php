435c27fcc52e5132c4d9f111bbfd6e8eca998b13
Audrey Cole <audreycole88@yahoo.com>
2014-09-13 17:39:00 -0500

    made html/css for index/submit

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 	use Profiler\Personality\PersonalityFactory;
-
-	require_once("includes/header.php"); // TODO refactor
+	<?php require 'vendor/autoload.php'; ?>
 ?>
-
+<head>
+		<link rel="stylesheet" type="text/css" href="static/css/style.css">
+</head>
+	<h1 class="title"> College Personality Profiler</h1>
 <body>
 	<div class="container">
 
 		<?php
 			$school = (@$_GET['school']) ? $_GET['school'] : "";
 		?>
-
+		<!--college search bar in center of page-->
 		<form action="submit.php">
 			<input class="collegename" type="text" placeholder="What college should I generate a personality for?" name="school" list="schools" value="<?php echo $school; ?>" />
 			<input class="collegename-submit" type="submit" value="Search">
@@ -24,5 +26,7 @@
 		<?php 
 			$profile = PersonalityFactory::createProfileBySchoolName($school);
 		?>
-	</div>
+	
+
+
 </body>
--- submit.php
+++ submit.php
@@ -1,7 +1,16 @@
 <?php
 	use Profiler\PersonalityFactory;
+	require 'vendor/autoload.php';
+	
 	require_once("includes/header.php"); // TODO refactor
+?>
+<head>
+		<link rel="stylesheet" type="text/css" href="static/css/style.css">
+</head>
 
+	<h1 class="title"> College Personality Profiler</h1>
+
+<?php
 	$schoolname =  $_GET["school"];
 	echo $schoolname;
 ?>
