38e327cb49d812d60ec0d5b8ec84b5a2e6b1ef11
Audrey Cole <audreycole88@yahoo.com>
2014-09-14 01:37:10 -0500

    added a pretty person silhouette!

--- submit.php
+++ submit.php
@@ -98,6 +98,7 @@
 		<section>
 			<blockquote>
 				<p id="introQuote" class="triangle-border left"> 
+					<img src="person.png" class="person-picture">
 					<strong>Hi!</strong> I go to <strong>WashU</strong>, but you might know it is as <strong>Washington University in St. Louis</strong>.
 				</p>
 			</blockquote>
@@ -108,8 +109,10 @@
 			It rains a ton. It snows a ton. And it gets really hot in the summer. <br> <br> <br>
 			<blockquote>
 				<p  id="weatherQuote" class="triangle-border left"> 
+					<img src="person.png" class="person-picture">
 					Wear snow boots!
 				</p>
+				<br>
 			</blockquote>
 		</section>
 
