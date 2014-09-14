696968c9033d7a132e537f9ceb4b95c3d0d9979b
Ben <ben@stolovitz.com>
2014-09-13 22:13:10 -0500

    basic tweaks

--- submit.php
+++ submit.php
@@ -1,20 +1,105 @@
 <?php
 	use Profiler\Personality\PersonalityFactory;
+	require 'vendor/autoload.php';
 
-	require_once("includes/header.php"); // TODO refactor
-
-	$schoolname =  $_GET["school"];
-	$personality = PersonalityFactory::createProfileBySchoolName($schoolname);
+	$school =  $_GET["school"];
+	$personality = PersonalityFactory::createProfileBySchoolName($school);	
 ?>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="static/css/style.css">
 
-<body>
+
+</head>
+<body class="submit">
 	<div class="container">
+		<header>
+			<h1>College Personality Profiler</h1>
+			<h2>We figure out what it's like to go to college so you don't have to. Yet.</h2>
+
+			<form class="search" action="submit.php">
+				<span>
+					<input class="collegename" 
+						type="text" 
+						placeholder="What college should I generate a personality for?"
+						name="school"
+						list="schools"
+						value="<?php echo $school; ?>" />
+				</span>
+				<input class="collegename-submit" type="submit" value="Search">
+
+				<datalist id="schools">
+					<option value="Washington University in St. Louis" />
+					<option value="Harvard University" />
+					<!-- TODO add all universities -->
+				</datalist>
+			</form>
+
+			<h1 class="college"><?php echo $_GET["school"]; ?></h1>
+		</header>
+
+		<section>
+			<blockquote>
+				<strong>Hi!</strong> I go to <strong>WashU</strong>, but you might know it is as <strong>Washington University in St. Louis</strong>.
+			</blockquote>
+
+			[Map Embed]
+
+			<img class="instagrampreview" src="<?php echo $personality->getInstagramUrl(); ?>">
+			<img class="instagrampreview" src="<?php echo $personality->getInstagramUrl(); ?>">
+			<img class="instagrampreview" src="<?php echo $personality->getInstagramUrl(); ?>">
+		</section>
+
+		<section>
+			<h2>Location</h2>
+
+			<blockquote>
+				Wear snow boots!
+			</blockquote>
+
+			It's in the midwest - in <strong>St. Louis, Missouri</strong>, to be precise - so (of course) the weather is absolutely terrible. 
+			It rains a ton. It snows a ton. And it gets really hot in the summer.
+		</section>
+
+
+
+		<h1>OLD DELETE FROM HERE ON</h1>
+		<div class="upperPictureBlock">
+			<h2 class="collegeNameHeader"><?php 
+				echo $_GET["school"];
+			?></h2><hr>
+			<h1 class="title"> College Personality Profiler </h1>
+				<!--college search bar in center of page-->
+				<div style="margin: 0 auto" align=center> 
+					<form action="submit.php">
+						<input class="collegename" type="text" placeholder="What college should I generate a personality for?" name="school" list="schools" value=
+						<input class="collegename-submit" type="submit" value="Search">
+
+						<datalist id="schools">
+							<option value="Washington University in St. Louis" />
+							<option value="Harvard University" />
+						</datalist>
+					</form>
+				</div>
+		</div>
+		<div class="lowerBlock">
+			<p class="triangle-border left"> 
+				Hi there I'm from WashU and I'm a Unicorn.
+			</p>
+			<p class="collegeDescription"> 
+				Oh hey there. I'm a student from WashU ;). I'm super smart, I sing in an acapella group, and I've been hacking for 40 hours at WUHack. My blood basically consists of caffeine right now. <br> <br>
+			</p>
+			<h2 class="location"> Location: St. Louis</h2> <hr>
+			<p class="triangle-border left"> 
+				hello there...
+			</p>
+		</div>
+
 		<p>The typical student who goes to <?php echo $schoolname ?> will be living near the following coordinates:</p>
 		Latitude: <?php echo $personality->getLatitude(); ?> <br>
 		Longitude <?php echo $personality->getLongitude(); ?> <br>
 		<p>Here's a photo of what's happening around campus: </p>
-		<p><img class="instagrampreview" src="<?php echo $personality->getInstagramUrl(); ?>"></p>
-	</div>
-
-	
+		<p></p>
+	</div>	
 </body>
+</html>
